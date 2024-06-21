93406115817b0513989c4ff9f00a0ef0bddcd2ae
baruc <barucorellana@gmail.com>
2024-06-21 11:46:27 -0600

    test(customer controller): add unit test for get all customers method

---
+++ orders-management/src/test/java/com/orders_management/web/controller/CustomerControllerTest.java
@@ -0,0 +1,35 @@
+package com.orders_management.web.controller;
+
+import com.orders_management.domain.service.CustomerService;
+import com.orders_management.persistence.entity.CustomerEntity;
+import com.orders_management.persistence.repository.CustomerRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.http.ResponseEntity;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class CustomerControllerTest {
+
+    CustomerRepository customerRepository = Mockito.mock(CustomerRepository.class);
+    CustomerService customerService = new CustomerService(customerRepository);
+    CustomerController customerController = new CustomerController(customerService);
+
+    @BeforeEach
+    public void createCustomers(){
+        List<CustomerEntity> customers =  new ArrayList<CustomerEntity>();
+        customers.add(new CustomerEntity(1,"Helm de México", null));
+        customers.add(new CustomerEntity(2,"Ingredion México", null));
+        customers.add(new CustomerEntity(3,"Gapelli", null));
+        customers.add(new CustomerEntity(4,"AGR Representaciones", null));
+        Mockito.when(customerRepository.findAll()).thenReturn(customers);
+    }
+    @Test
+    void get_all_customers(){
+        Assertions.assertEquals(ResponseEntity.ok(customerRepository.findAll()), customerController.getAll());
+    }
+}
