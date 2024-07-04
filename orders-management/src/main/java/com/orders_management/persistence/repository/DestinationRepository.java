7a78f137c61ad438f4f5071120b8f74633fafeaf
baruc <barucorellana@gmail.com>
2024-07-04 01:57:31 -0600

    feat(destination entity): add create, read, update and delete methods
    methods added:
    - getById
    - getAll
    - save
    - update
    - delete

--- orders-management/src/main/java/com/orders_management/persistence/repository/DestinationRepository.java
+++ orders-management/src/main/java/com/orders_management/persistence/repository/DestinationRepository.java
@@ -1,2 +1,12 @@
-package com.orders_management.persistence.repository;public interface DestinationRepository {
+package com.orders_management.persistence.repository;
+
+import com.orders_management.persistence.entity.DestinationEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.ListCrudRepository;
+
+public interface DestinationRepository extends ListCrudRepository<DestinationEntity, Integer> {
+    @Query(value = "SELECT * FROM destination WHERE id_destination = :idDestination", nativeQuery = true)
+    DestinationEntity getDestinationById(int idDestination);
+    @Query(value = "SELECT * FROM destination WHERE destination = :destination", nativeQuery = true)
+    DestinationEntity getByDestination(String destination);
 }
--- orders-management/src/main/java/com/orders_management/web/controller/DestinationController.java
+++ orders-management/src/main/java/com/orders_management/web/controller/DestinationController.java
@@ -1,2 +1,47 @@
-package com.orders_management.web.controller;public class DestinationController {
+package com.orders_management.web.controller;
+
+import com.orders_management.domain.service.DestinationService;
+import com.orders_management.persistence.entity.DestinationEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("destination")
+public class DestinationController {
+    public final DestinationService destinationService;
+
+    @Autowired
+    public DestinationController(DestinationService destinationService) {
+        this.destinationService = destinationService;
+    }
+    @GetMapping("/id/{idDestination}")
+    public ResponseEntity<DestinationEntity> getDestinationById(@PathVariable("idDestination") int idDestination){
+        return ResponseEntity.ok(this.destinationService.getDestinationById(idDestination));
+    }
+    @GetMapping("/all")
+    public ResponseEntity<List<DestinationEntity>> getAllDestinations(){
+        return ResponseEntity.ok(this.destinationService.getAll());
+    }
+    @PostMapping("/new")
+    public ResponseEntity<DestinationEntity> saveDestination(@RequestBody DestinationEntity destination){
+        if(!this.destinationService.exits(destination.getIdDestination())){
+            return ResponseEntity.ok(this.destinationService.save(destination));
+        }return ResponseEntity.badRequest().build();
+    }
+    @PutMapping("/update")
+    public ResponseEntity<DestinationEntity> updateDestination(@RequestBody DestinationEntity destination){
+        if(this.destinationService.exits(destination.getIdDestination())){
+            return ResponseEntity.ok(this.destinationService.save(destination));
+        }return ResponseEntity.badRequest().build();
+    }
+    @DeleteMapping("/delete/{idDestination}")
+    public ResponseEntity<DestinationEntity> deleteDestination(@PathVariable("idDestination") int idDestination){
+        if(this.destinationService.exits(idDestination)){
+            this.destinationService.delete(idDestination);
+            return ResponseEntity.ok().build();
+        } return ResponseEntity.badRequest().build();
+    }
 }
