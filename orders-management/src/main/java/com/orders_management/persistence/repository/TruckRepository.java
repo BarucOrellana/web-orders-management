e396310a72ea1e8877f191889b73e5b5a2acb892
baruc <barucorellana@gmail.com>
2024-07-09 19:17:23 -0600

    feat(truck entity): create, read, update and delete methods methods added: - getByStatus - getByMaintenanceStatus - getAll - save - update - delete

--- orders-management/src/main/java/com/orders_management/persistence/repository/TruckRepository.java
+++ orders-management/src/main/java/com/orders_management/persistence/repository/TruckRepository.java
@@ -1,2 +1,14 @@
-package com.orders_management.persistence.repository;public interface TruckRepository {
+package com.orders_management.persistence.repository;
+
+import com.orders_management.persistence.entity.TruckEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.ListCrudRepository;
+
+import java.util.List;
+
+public interface TruckRepository extends ListCrudRepository<TruckEntity, Integer> {
+    @Query(value = "SELECT * FROM truck WHERE available = :status", nativeQuery = true)
+    List<TruckEntity> getAvailableTruck(boolean status);
+    @Query(value = "SELECT * FROM truck WHERE maintenance = :maintenance", nativeQuery = true)
+    List<TruckEntity> getMaintenanceTruck(boolean maintenance);
 }
