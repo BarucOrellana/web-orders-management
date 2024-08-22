d9fec479b7e80559b7579cc63ed48fd92750b9db
baruc <barucorellana@gmail.com>
2024-08-22 16:45:03 -0600

    add redis configuration

--- orders-management/src/main/java/com/orders_management/web/config/RedisConfig.java
+++ orders-management/src/main/java/com/orders_management/web/config/RedisConfig.java
@@ -1,2 +1,25 @@
-package com.orders_management.web.config;public class RedisConfig {
+package com.orders_management.web.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@EnableCaching
+public class RedisConfig {
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration configuration = new RedisStandaloneConfiguration("localhost", 6379);
+        return new LettuceConnectionFactory(configuration);
+    }
+    @Bean
+    RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        return template;
+    }
 }
