5ecd45c48d7ad6380bac85eea1c6198ca7deb2d3
baruc <barucorellana@gmail.com>
2024-10-19 11:25:12 -0600

    add API documentation configuration

--- orders-management/src/main/java/com/orders_management/web/config/OpenApiConfig.java
+++ orders-management/src/main/java/com/orders_management/web/config/OpenApiConfig.java
@@ -1,17 +1,17 @@
 package com.orders_management.web.config;
 
-/*import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
-public class SwaggerConfig {
+public class OpenApiConfig {
         @Bean
         GroupedOpenApi publicApi(){
                 return GroupedOpenApi.builder().group("public-apis").pathsToMatch("/**").build();
@@ -23,4 +23,4 @@ public class SwaggerConfig {
                         .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                         .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
         }
-}*/
+}
